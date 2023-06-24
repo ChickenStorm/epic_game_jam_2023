dfd81600a8460bfb24cb9e77127058d4fda771f0
lfatton <lfatton@LAPTOP-5LDR2FI6>
2023-06-24 16:02:48 +0200

    can quit with ESC

--- main.gd
+++ main.gd
@@ -9,3 +9,9 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+	
+func _input(event):
+	if event is InputEventKey and event.pressed:
+		if event.keycode == KEY_ESCAPE:
+			get_tree().quit()
--- menu/MainMenu.gd
+++ menu/MainMenu.gd
@@ -11,8 +11,14 @@ func _process(delta):
 	pass
 
 func _on_start_pressed():
-	get_tree().change_scene_to_file("res://world/world.tscn")
+	get_tree().change_scene_to_file("res://main.tscn")
 
 
 func _on_quit_pressed():
 	get_tree().quit()
+	
+	
+func _input(event):
+	if event is InputEventKey and event.pressed:
+		if event.keycode == KEY_ESCAPE:
+			get_tree().quit()

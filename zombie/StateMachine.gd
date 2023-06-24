26a123aed8a3ab5655866a7a7af13a6f1bf666aa
MattD <matt@le-dahu-stud.io>
2023-06-25 01:18:10 +0200

    red?

--- zombie/StateMachine.gd
+++ zombie/StateMachine.gd
@@ -1,6 +1,9 @@
 extends Node
 
 @onready var currentState = get_node('Roam')
+@onready var zombie = get_owner()
+@onready var angryMaterial = preload("res://zombie/angry.tres")
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
@@ -9,4 +12,5 @@ func _physics_process(delta):
 func _on_aggro_zone_body_entered(body):
 	currentState = get_node('Attack')
 	currentState.target = body
+	$"../ZombieForm".set_surface_override_material(0, angryMaterial)
 	print("agrougrou")

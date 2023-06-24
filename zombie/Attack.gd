c97853431d16a5a88ceab1237cbc6c6f3614a180
MattD <matt@le-dahu-stud.io>
2023-06-24 23:14:41 +0200

    wip

--- zombie/Attack.gd
+++ zombie/Attack.gd
@@ -1,18 +1,16 @@
-extends Node
+extends NPCState
+class_name NPCAttack
 
 var rng = RandomNumberGenerator.new()
-var zombie
-var player
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	await owner.ready
 	zombie = owner
-	player = Vector3(0, 0, 0)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	zombie.look_at(player,Vector3.UP)
+func process(delta):
+	zombie.look_at(target.position,Vector3.UP)
 	zombie.translate(Vector3(0,0,delta * 5))
 	
